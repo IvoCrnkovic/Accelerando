9d98110e17cd7b294a7f15f94a12cc29b5470cd7
Brendan Chou <brendanchou@msn.com>
2012-06-06 16:54:18 -0400

    6/6/12 daily

---
+++ PeriodicTweetPuller/src/TrendingTweetPuller.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+import twitter4j.*;
+
+public class TrendingTweetPuller {
+	
+	public static void main(String[] args) throws TwitterException {
+		
+		System.out.println("-BEGIN-\n");
+		
+		TwitterFactory twitterFactory = new TwitterFactory();
+        Twitter twitter = twitterFactory.getInstance();
+        ResponseList<Trends> trendsList = twitter.getDailyTrends();
+        Query query;
+        List<Tweet> tweets;
+    	int trendsListSize, hourlyTrendArraySize, resultsSize;
+    	
+        trendsListSize = trendsList.size();
+        for(int i = 0; i < trendsListSize; i++)
+        {
+        	System.out.print("\nHour " + i + ": ");
+        	Trends hourlyTrends = trendsList.get(i);
+			Trend[] hourlyTrendArray = hourlyTrends.getTrends();
+			hourlyTrendArraySize = hourlyTrendArray.length;
+			for(int j = 0; j < hourlyTrendArraySize; j++)
+			{
+				String trendName = hourlyTrendArray[j].getName();
+				query = new Query(trendName);
+		        query.setRpp(2);
+		        query.setLang("en");
+				tweets = twitter.search(query).getTweets();
+				resultsSize = tweets.size();
+				for(int k = 0; k < resultsSize; k++)
+					addTweetToTree(new SuperTweet(tweets.get(k), trendName));
+				System.out.print(trendName + ", ");
+			}
+        }
+        
+        System.out.println("\n\n\n-COMPLETE-");
+        
+	}
+	
+	private static void addTweetToTree(SuperTweet superTweet)
+	{
+		
+	}
+
+}
