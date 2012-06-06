f2382623063b77dc86901a6c286012102c9ea6dc
Ivo Crnkovic-Rubsamen <Ivo.Crnkovic@gmail.com>
2012-06-06 00:10:39 -0400

    Add Ivo

---
+++ Ivo/TweetSetEvaluator/src/testing.java
@@ -0,0 +1,13 @@
+
+public class testing {
+	
+	public static void main (String [] args)
+	{
+		float [] weightings = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+		float [] polarizations = {1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5};
+		
+		System.out.println(SetEvaluator.SetEvaluator(weightings, polarizations));
+	}
+
+
+}
